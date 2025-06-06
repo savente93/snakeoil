7d655ada3310eacb09b8d791b4c70edbee75df4a
Sam Vente <savente93@proton.me>
2025-06-06 13:30:08 +0200

    test: add zola integration test
    will need a zola binary installed, will need to figure out how to manage
    this later, e.g. in ci

--- tests/main.rs
+++ tests/main.rs
@@ -25,3 +25,41 @@ fn test_cli_with_all_options() -> Result<()> {
 
     Ok(())
 }
+#[test]
+fn test_cli_with_zola() -> Result<()> {
+    let tempdir = tempdir()?;
+
+    let target_dir = tempdir.path().join("zola_test_site");
+
+    // I'm too lazy to implement copying the file tree in rust
+    let _ = Command::new("cp")
+        .arg("-r")
+        .arg("tests/zola_test_site/")
+        .arg(&target_dir)
+        .assert();
+
+    let mut cmd = Command::cargo_bin("snakedown")?;
+    cmd.arg("tests/test_pkg")
+        .arg(target_dir.join("content"))
+        .arg("--skip-undoc")
+        .arg("--skip-private")
+        .arg("-e")
+        .arg("test_pkg/excluded_file.py")
+        .arg("--exclude")
+        .arg("test_pkg/excluded_module")
+        .arg("--format")
+        .arg("zola")
+        .arg("-vv");
+    let snakedown_assertion = cmd.assert();
+
+    snakedown_assertion.success();
+
+    let zola_cmd_assert = Command::new("zola")
+        .current_dir(&target_dir)
+        .arg("build")
+        .assert();
+
+    zola_cmd_assert.success();
+
+    Ok(())
+}
