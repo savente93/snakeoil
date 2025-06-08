624202d0943d87bd3654caa7d12536bda69adabd
Sam Vente <savente93@proton.me>
2025-06-08 14:18:27 +0200

    fix: remove syntax_error.py from output
    this should be tested seperately

--- tests/test_pkg/_private/__init__.py
+++ tests/test_pkg/_private/__init__.py
@@ -3,7 +3,3 @@ The _private subpackage
 
 This subpackage contains internal modules and functions intended for internal use.
 """
-
-from .internals import calculate_secret_value, InternalHelper
-
-__all__ = ["calculate_secret_value", "InternalHelper"]
