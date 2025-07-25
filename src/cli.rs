0fa8aa807d09b8ebed2e78df8a558bebf6b8e691
Sam Vente <savente93@proton.me>
2025-07-25 21:50:43 +0200

    feat: allow config file passed through cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,12 @@
+use color_eyre::Result;
 use std::path::PathBuf;
 
 use clap::Parser;
 use clap_verbosity_flag::{LogLevel, Verbosity, VerbosityFilter};
-use snakedown::render::SSG;
+use snakedown::{
+    config::{Config, ConfigBuilder},
+    render::SSG,
+};
 
 #[allow(dead_code)]
 pub struct CustomLogLevel {}
@@ -27,17 +31,56 @@ impl LogLevel for CustomLogLevel {
     }
 }
 
+pub fn resolve_runtime_config(args: Args) -> Result<Config> {
+    let mut config_builder = ConfigBuilder::default();
+
+    if let Some(config_file_path) = discover_config_file(args.config_file) {
+        let file_config_builder = ConfigBuilder::from_path(&config_file_path)?;
+        config_builder = config_builder.merge(file_config_builder);
+    }
+
+    let cli_args_builder = ConfigBuilder::default()
+        .with_output_dir(args.output_dir)
+        .with_pkg_path(args.pkg_path)
+        .with_skip_undoc(if args.skip_undoc { Some(true) } else { None })
+        .with_skip_private(if args.skip_private { Some(true) } else { None })
+        .with_exclude(args.exclude)
+        .with_ssg(args.ssg);
+
+    config_builder = config_builder.merge(cli_args_builder);
+
+    config_builder.build()
+}
+
+pub fn discover_config_file(arg_config_path: Option<PathBuf>) -> Option<PathBuf> {
+    let mut candidates = vec![];
+
+    if let Some(args_path) = arg_config_path {
+        candidates.push(args_path);
+    }
+
+    candidates.push(PathBuf::from("snakedown.toml"));
+    candidates.push(PathBuf::from("$HOME/.config/snakedown/snakedown.toml"));
+    candidates
+        .into_iter()
+        .find(|candidate| candidate.exists() && candidate.is_file())
+}
+
 #[derive(Parser)]
 #[command(version, about, long_about= None)]
 pub struct Args {
     #[command(flatten)]
     pub verbose: Verbosity,
 
-    #[arg(default_value = ".")]
-    pub pkg_path: PathBuf,
+    /// The path of the root of the package
+    pub pkg_path: Option<PathBuf>,
 
-    #[arg(default_value = "_build")]
-    pub output_dir: PathBuf,
+    /// The directory where to put the rendered docs
+    pub output_dir: Option<PathBuf>,
+
+    /// The path to the configuration file
+    #[arg(long, short)]
+    pub config_file: Option<PathBuf>,
 
     /// Do not render undocumented functions, classes or modules
     #[arg(long, default_value_t = false)]
@@ -47,13 +90,13 @@ pub struct Args {
     #[arg(long, default_value_t = false)]
     pub skip_private: bool,
 
-    /// any files that should be excluded, can be file or directories and specific multiple times but currently globs are not supported
+    /// Any files that should be excluded, can be file or directories and specific multiple times but currently globs are not supported
     #[arg(short, long)]
-    pub exclude: Vec<PathBuf>,
+    pub exclude: Option<Vec<PathBuf>>,
 
     /// What format to render the front matter in, (zola, hugo, plain markdown, etc.)
-    #[arg(short, long, value_enum, default_value_t = SSG::Markdown)]
-    pub ssg: SSG,
+    #[arg(short, long, value_enum)]
+    pub ssg: Option<SSG>,
 }
 
 #[cfg(test)]
@@ -90,13 +133,13 @@ mod tests {
 
     #[test]
     fn test_args_defaults() -> Result<()> {
-        let args = Args::parse_from(["mybin"]);
-        assert_eq!(args.pkg_path, PathBuf::from("."));
-        assert_eq!(args.output_dir, PathBuf::from("_build"));
+        let args = Args::parse_from(["snakedown"]);
+        assert!(args.pkg_path.is_none());
+        assert!(args.output_dir.is_none());
         assert!(!args.skip_undoc);
         assert!(!args.skip_private);
-        assert!(args.exclude.is_empty());
-        assert_eq!(args.ssg, SSG::Markdown);
+        assert!(args.exclude.is_none());
+        assert!(args.ssg.is_none());
         Ok(())
     }
 
@@ -117,28 +160,28 @@ mod tests {
             "-v",
             "-v",
         ]);
-        assert_eq!(args.pkg_path, PathBuf::from("src/pkg"));
-        assert_eq!(args.output_dir, PathBuf::from("dist"));
+        assert_eq!(args.pkg_path, Some(PathBuf::from("src/pkg")));
+        assert_eq!(args.output_dir, Some(PathBuf::from("dist")));
         assert!(args.skip_undoc);
         assert!(args.skip_private);
         assert_eq!(
             args.exclude,
-            vec![
+            Some(vec![
                 PathBuf::from("path/to/exclude1"),
                 PathBuf::from("path/to/exclude2")
-            ]
+            ])
         );
         // Verbosity should be INFO with -v -v (test indirectly)
         let level = args.verbose.log_level();
         assert_eq!(level, Some(Level::Info));
-        assert_eq!(args.ssg, SSG::Markdown);
+        assert_eq!(args.ssg, Some(SSG::Markdown));
         Ok(())
     }
 
     #[test]
     fn test_args_exclude_short_flag() -> Result<()> {
         let args = Args::parse_from(["mybin", "-e", "excluded"]);
-        assert_eq!(args.exclude, vec![PathBuf::from("excluded")]);
+        assert_eq!(args.exclude, Some(vec![PathBuf::from("excluded")]));
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 use color_eyre::eyre::Result;
-use snakedown::{
-    render::{
-        SSG,
-        formats::{Renderer, md::MdRenderer, zola::ZolaRenderer},
-    },
-    render_docs,
-};
+use snakedown::render_docs;
 use tracing::subscriber::set_global_default;
 
 mod cli;
 
-use crate::cli::Args;
+use crate::cli::{Args, resolve_runtime_config};
 use clap::Parser;
 
 #[allow(clippy::missing_errors_doc)]
@@ -25,18 +19,14 @@ async fn main() -> Result<()> {
 
     set_global_default(subscriber)?;
 
-    let renderer: Box<dyn Renderer> = match args.ssg {
-        SSG::Markdown => Box::new(MdRenderer::new()),
-        SSG::Zola => Box::new(ZolaRenderer::new()),
-    };
-
+    let config = resolve_runtime_config(args)?;
     render_docs(
-        &args.pkg_path,
-        &args.output_dir,
-        args.skip_private,
-        args.skip_undoc,
-        args.exclude,
-        &renderer,
+        &config.pkg_path,
+        &config.output_dir,
+        config.skip_private,
+        config.skip_undoc,
+        config.exclude,
+        &config.renderer,
     )?;
 
     Ok(())
